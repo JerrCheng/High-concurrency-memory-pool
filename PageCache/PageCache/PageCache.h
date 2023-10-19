87584fda5769c80b9d3c0075eee0f426fd9d7dfb
jerry-cheng123 <chr17739159896@163.com>
2023-10-19 10:59:57 +0800

    PageCache

---
+++ PageCache/PageCache/PageCache.h
@@ -0,0 +1,26 @@
+﻿#pragma once
+
+#include "Common.h"
+
+class PageCache
+{
+public:
+	static PageCache* GetInstance()
+	{
+		return &_sInst;
+	}
+
+	// ȡһKҳspan
+	Span* NewSpan(size_t k);
+
+	std::mutex _pageMtx;
+private:
+	SpanList _spanLists[NPAGES];
+
+	PageCache()
+	{}
+	PageCache(const PageCache&) = delete;
+
+
+	static PageCache _sInst;
+};
